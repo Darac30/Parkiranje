434c4ba406d9218a997d7ecb6601d1429c31e46c
Darac30 <117777192+Darac30@users.noreply.github.com>
2025-08-14 20:40:10 +0200

    Postavljanje rada s bazom

---
+++ BACKEND/Data/EdunovaContext.cs
@@ -0,0 +1,15 @@
+﻿using BACKEND.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BACKEND.Data
+{
+    public class EdunovaContext : DbContext
+    {
+        public EdunovaContext(DbContextOptions<EdunovaContext> options) : base(options)
+        { 
+        // ovdje se mogu fino postaviti opcije, ali ne za sada
+        }
+
+        public DbSet<Razina> Razine { get; set; } // zbog ovoga ovdje se Razine se tablica zove u množini
+    }
+}
